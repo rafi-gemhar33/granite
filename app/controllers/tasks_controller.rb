1df308de7ac52d0f1f770085626a340c79b0d84e
Rafi Gemhar <gemhar@zoho.com>
2022-09-05 20:55:27 +0530

    35. Added ability to update a task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class TasksController < ApplicationController
-  respond_to :html, :xml, :json
+  before_action :load_task!, only: %i[show update]
 
   def index
     tasks = Task.all
@@ -9,8 +9,7 @@ class TasksController < ApplicationController
   end
 
   def show
-    task = Task.find_by!(slug: params[:slug])
-    respond_with_json({ task: task })
+    respond_with_json({ task: @task })
   end
 
   def create
@@ -19,7 +18,16 @@ class TasksController < ApplicationController
     respond_with_success t("successfully_created")
   end
 
+  def update
+    @task.update!(task_params)
+    respond_with_success t("successfully_updated")
+  end
+
   private
+
+    def load_task!
+      @task = Task.find_by!(slug: params[:slug])
+    end
 
     def task_params
       params.require(:task).permit(:title)
--- app/javascript/src/apis/tasks.js
+++ app/javascript/src/apis/tasks.js
@@ -3,7 +3,11 @@ import axios from "axios";
 const list = () => axios.get("/tasks");
 const create = payload => axios.post("/tasks", { task: payload });
 const show = slug => axios.get(`/tasks/${slug}`);
+const update = ({ slug, payload }) =>
+  axios.put(`/tasks/${slug}`, {
+    task: payload,
+  });
 
-const tasksApi = { list, create, show };
+const tasksApi = { list, create, show, update };
 
 export default tasksApi;
