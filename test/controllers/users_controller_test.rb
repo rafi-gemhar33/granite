bee7e3a4b5e1f9161095d8a42ebbaf9a1adc3fe1
Rafi Gemhar <gemhar@zoho.com>
2022-09-09 03:19:55 +0530

    59 .Added controller tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,8 +3,45 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "should get index" do
-  #   get users_index_url
-  #   assert_response :success
-  # end
+  def setup
+    Rails.application.reload_routes!
+    user = create(:user)
+    @headers = headers(user)
+  end
+
+  def test_should_list_all_users
+    get users_path, headers: @headers
+    assert_response :success
+
+    response_json = response.parsed_body
+    assert_equal response_json["users"].length, User.count
+  end
+
+  def test_should_signup_user_with_valid_credentials
+    post users_path, params: {
+      user: {
+        name: "Sam Smith",
+        email: "sam@example.com",
+        password: "welcome",
+        password_confirmation: "welcome"
+      }
+    }, headers: @headers
+    assert_response :success
+    response_json = response.parsed_body
+    assert_equal response_json["notice"], t("successfully_created", entity: "User")
+  end
+
+  def test_shouldnt_signup_user_with_invalid_credentials
+    post users_path, params: {
+      user: {
+        name: "Sam Smith",
+        email: "sam@example.com",
+        password: "welcome",
+        password_confirmation: "not matching confirmation"
+      }
+    }, headers: @headers
+
+    assert_response :unprocessable_entity
+    assert_equal response.parsed_body["error"], "Password confirmation doesn't match Password"
+  end
 end
